cede09063756c09692207b6ce5b1e6303f5bd1a5
JMarguery <127878858+JMarguery@users.noreply.github.com>
2024-02-08 11:23:25 +0100

    ParticleFirework added

---
+++ src/firework/ParticleTrail.js
@@ -0,0 +1,75 @@
+class ParticleTrail extends ParticlePhysic{
+
+    constructor(color,radius,posX,posY,velX,velY,maxttl,gravity,trailLength){
+        super(
+            color,
+            radius,
+            posX,
+            posY,
+            velX,
+            velY,
+            maxttl+trailLength,
+            gravity);
+        this.trailLength = trailLength;
+
+        this.isMovable = true;
+
+        this.oldPositions = [];
+
+        this.oldPositions.push({x:posX,y:posY});
+    }
+
+    draw(){
+        this.move();
+        super.draw();
+    }
+
+    move(){
+        if(!this.isMovable){
+            if (this.oldPositions.length > 0){
+                this.updateOldPositions();
+                this.oldPositions.pop();
+                this.createTrail();
+            }
+        }else {
+            if((this.position.x > CanvasManager.canvas.width || this.position.x < 0 || this.position.y > CanvasManager.canvas.height || this.position.y < 0 ) || this.ttl>=this.maxttl-this.trailLength){
+                this.isMovable = false;
+                this.updateOldPositions();
+                this.createTrail();
+            }
+            else {
+                this.updateOldPositions();
+                this.createTrail();
+            }
+        }
+    }
+
+    tickTTL(){
+        this.ttl+=1;
+        return ((((this.ttl<this.maxttl) || this.oldPositions.length==0)));
+    }
+
+    updateOldPositions(){
+        if (this.oldPositions.length < this.trailLength){
+            let x = this.position.x;
+            let y = this.position.y;
+            this.oldPositions.unshift({x, y});
+        }else{
+            let x = this.position.x;
+            let y = this.position.y;
+            this.oldPositions.unshift({x, y});
+            this.oldPositions.pop(); // Supprime le dernier élément du tableau pour maintenir la longueur
+        }
+    }
+
+    createTrail(){
+        for (let i = 0; i < this.oldPositions.length; i++) {
+            const size = 1 - (i + 1) / this.oldPositions.length; // change petit a petit la taille
+            CanvasManager.context.fillStyle = this.color;
+            CanvasManager.context.beginPath();
+            CanvasManager.context.arc(this.oldPositions[i].x, this.oldPositions[i].y, this.radius * size, 0, Math.PI * 2, true);
+            CanvasManager.context.closePath();
+            CanvasManager.context.fill();
+        }
+    }
+}
---
+++ src/firework/utils.js
@@ -0,0 +1,30 @@
+
+function getRandomInt(min,max) {
+    return Math.floor(Math.random() * (max-min)+min);
+}
+
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getRandomRGBA(){
+    let red = getRandomInt(0, 255);
+    let green = getRandomInt(0, 255);
+    let blue = getRandomInt(0, 255);
+    let alpha = 1; // You specified alpha as 1, which means fully opaque
+
+    return `rgba(${red},${green},${blue},${alpha})`;
+}
+
+// Fonction pour générer des directions symétriques autour de la direction initiale
+function genererDirectionsSymetriques(nombreParticules, angleDirectionInitiale, angleDispersion) {
+    let directions = [];
+    const anglePas = (2 * Math.PI) / nombreParticules;
+    for (let i = 0; i < nombreParticules; i++) {
+        const angle = angleDirectionInitiale + anglePas * i;
+        const directionX = Math.cos(angle) * Math.cos(angleDispersion) - Math.sin(angle) * Math.sin(angleDispersion);
+        const directionY = Math.sin(angle) * Math.cos(angleDispersion) + Math.cos(angle) * Math.sin(angleDispersion);
+        directions.push({ x: directionX, y: directionY });
+    }
+    return directions;
+}
