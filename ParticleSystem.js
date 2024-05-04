d23e85ff56bf48506cf8b16f4b7b8e7b4e352cc1
Mateusz-Birembaut <60432147+Mateusz-Birembaut@users.noreply.github.com>
2024-05-04 17:21:24 +0200

    ParticleSystem.js

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -16,9 +16,41 @@ class ParticleSystem {
 
     }
 
+    static createFireParticles(nb_particles, radiusParticles, opacityParticles){
+        this.particles = [];
+        for (let i = 0; i < nb_particles; i++) {
+            ParticleSystem.particles.push(new ParticleFire(
+                `rgba(255,255,0,${opacityParticles})`,
+                radiusParticles,
+                getRandomFloat((CanvasManager.canvas.width/2)-50,(CanvasManager.canvas.width/2)+50),
+                CanvasManager.canvas.height*3/4,
+                getRandomInt(Simulation.ttlMin, Simulation.ttlMax),
+                Simulation.speedFactor,
+                Simulation.spreadX,
+                Simulation.spreadY
+            ));
+        }
+    }
+
+    static createSmokeParticles(nb_particles, radiusParticles, opacityParticles){
+        this.particles = [];
+        for (let i = 0; i < nb_particles; i++) {
+            ParticleSystem.particles.push(new ParticleSmoke(
+                `rgba(255,255,255,${opacityParticles})`,
+                radiusParticles,
+                CanvasManager.canvas.width/2,
+                CanvasManager.canvas.height*3/4,
+                getRandomInt(Simulation.ttlMin, Simulation.ttlMax),
+                Simulation.speedFactor,
+                Simulation.spreadX,
+                Simulation.spreadY
+            ));
+        }
+    }
+
     static pass() {
         for (let particle of this.particles) {
-            if (particle instanceof ParticleWindMap) {
+            if (particle instanceof Particle) {
                 if (!particle.tickttl()){
                     particle.setPositionToSpawn();
                 }
